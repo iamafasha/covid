bc20f8c23827b99a0521ec102511560126440786
Afasha Isakiye <afashaisakiye@gmail.com>
2020-04-09 06:35:58 +0300

    Moved to webpack

--- src/index.js
+++ src/index.js
@@ -3,5 +3,6 @@ import {render} from 'react-dom'
 import App from './App';
 import 'bootstrap/dist/css/bootstrap.min.css'
 import  './style.scss';
-
+import * as serviceWorker from './serviceWorker';
 render(<App />,document.querySelector('#app'));
+serviceWorker.register();
